457805b4c5f4d462510eb60f76278c15c989518a
MariaGarciaB <licgarcia_psic@hotmail.com>
2023-01-31 19:16:06 -0600

    valor input

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 import validator from './validator.js';
 
+function validate() {
+  const cardnumber = document.getElementById("cardNumber").value;//por medio del btnValidate, se captura el valor en caja vacía
+  if (cardnumber === "") {
+    alert("Ingresa los números de tu tarjeta") //Marcar un error cuando no ingresa texto (Operadores lógicos)
+  } else {
+    console.log(cardnumber)
+    //isValid (cardnumber) // enviamos argumento a nueva funcion
+  }
+}
+
+const button = document.getElementById("btnValidate");// Llamar al boton
+button.addEventListener("click", validate);//Manejador de eventos, añade funcionalidad
+
+
+
 console.log(validator);
+
+
+
+// hasta este momento aún no hacemos algo
+//verificar los valores despues de dar click boton
+//buscar, alertas, interacción, actualizar, leer cosas en un div, mensajes a ususaros, ergc.
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,53 @@
 const validator = {
-  // ...
+
+  isValid: function(cardnumber) {// recibes el parametro
+    console.log(cardnumber)
+    const numberValid = cardnumber.split("");//separar los números en string individuales
+    console.log(numberValid)
+    const inverse = numberValid.reverse();//array en orden inverso
+    console.log(inverse)
+    const posicionesPares = [];
+    const posicionesImpares = [];
+    let a = 0;
+    let b = 0;
+    let c = 0;
+    for (let i = 0; i < inverse.length; i++) {//separar por posiciones
+      const array = inverse[i];
+      console.log(array)
+      if (i % 2 === 1){//selecciona pocisiones pares (operador modulo)
+        const multiplication = + (array*2);//multiplicar por 2 el valor de posiciones pares
+        console.log(multiplication)
+        posicionesPares.push(array);//valor pocisiones pares en array
+        if(multiplication >9){//seleccionar números mayores a 9
+          const dosDigitos = multiplication.toString().split("");//convertir a string mi true en mayores de 9 (suma pares)
+          const sumaDosDigitos = (Number(dosDigitos[0]) + Number(dosDigitos[1]));//Sumar ambos dígitos
+          console.log (dosDigitos)
+          console.log (sumaDosDigitos)
+          a = a + sumaDosDigitos;//sumar el resultado de todos los "sumaDosDigitos" se declara una variable fuera del for =0 para que comience a partir de ese número
+        }else{
+          b = b + multiplication;//suma pocisiones pares sin multiplicar
+        }
+      }
+      if(i % 2 === 0){//selecciona pocisiones impares
+        c = c + (Number(array));
+        posicionesImpares.push(array);//valor pocisiones impares en array
+      }
+    }
+    const sumaTotal = (a + b + c);//Suma total
+    if(sumaTotal %10 === 0){//si mi residual es =0, me da una tarjeta valida
+      alert ("Tarjeta Valida")
+    } else{
+      alert ("Tú tarjeta NO es valida. Por favor, ingresa nuevamente tu Card Number")
+    }
+    console.log(posicionesImpares)
+    console.log(posicionesPares)
+    console.log (c)//suma de posiciones impares
+    console.log (b)
+    console.log (a)
+    console.log (sumaTotal)//suma de todos los digitos agregando el resultado anterior
+  }
 };
 
 export default validator;
+
+//no va document, alert... aqui meto mis funciones que van a dar un valor
